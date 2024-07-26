7ee7a2580db22a48d30cbd749f79046fd69a3c5e
nived-s <nivedsreenilayam@gmail.com>
2024-07-26 21:37:01 +0530

    final product

---
+++ Expense-Tracker/src/expense-tracker/components/Form.tsx
@@ -0,0 +1,52 @@
+import { FieldValues, useForm } from "react-hook-form";
+
+
+
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be min 3 characters</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-lable">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
